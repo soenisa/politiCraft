9477bfa8e03dad55e1583fa0f30aa94d6453da4e
Senisa <senisa.s@gmail.com>
2016-02-06 23:49:34 -0500

    Added player counter + nouner

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var mcAPI = 'http://mcping.net/api/192.0.223.229';
 
+var noun = ['scrub', 'nooblet', 'paladin', 'noob', 'Supreme Court Justice', 'Donald Trump', 'cat', 'Jesse', 'creeper', 'misplaced semicolon']
+
 	function call() {
 		$.getJSON(mcAPI, function(json) {
 			console.log("JSON Data: " + json.online)
@@ -9,13 +11,20 @@ var mcAPI = 'http://mcping.net/api/192.0.223.229';
 				$("#players-panel.panel-heading").empty().append("There's no one online :(");
 				$("#players-list").empty();
 			}
-			else
+			else {
 				for(var i = 0; i < Object.keys(json.sample).length; i++) {
 					var name = json.sample[i].name;
 					console.log(name);
 					result.push($('<li class="list-group-item"></li>').text(name));
 				}
-			$("#players-panel").removeClass("panel-danger").addClass("panel-success");
-			$("#players-list").empty().append(result);
+
+				$("#players-panel").removeClass("panel-danger").addClass("panel-success");
+				$("#players-list").empty().append(result);
+				var population = result.length;
+				if(population > 1)
+					$("#info").empty().append("There are currently <b>" + population + " " + noun[Math.floor((Math.random() * noun.length))] + "s</b> on the server.");
+				else
+					$("#info").empty().append("There is currently <b>1 " + noun[Math.floor((Math.random() * noun.length))] + "</b> on the server.");
+			}
 		});
 	};
